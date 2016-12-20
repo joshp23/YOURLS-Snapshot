a960629d4614fe193cc946909bb50b0d45700e20
Josh Panter <joshu@unfettered.net>
2016-12-20 01:52:08 -0500

    Update plugin.php

--- snapshot/plugin.php
+++ snapshot/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Snapshot: Visual URL Preview
 Plugin URI: https://github.com/joshp23/YOURLS-Snapshot
 Description: Preview plugin with an image Cahche
-Version: 2.0.0
+Version: 2.0.1
 Author: Josh Panter <joshu@unfettered.net>
 Author URI: https://unfettered.net
 */
